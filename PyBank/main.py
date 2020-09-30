6718dde480bc9081d77a9d1d9457390118332aa0
doublealogic <cras.ajrobertson@gmail.com>
2020-09-29 23:56:12 -0400

    Updated script to calculate net total amount of profits and losses over the entire period.

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,7 +2,9 @@ import os
 import csv
 
 csvpath = os.path.join('..', 'PyBank', 'Resources', 'budget_data.csv')
+
 month_total = 0
+net_total = 0
 
 with open(csvpath) as budget_file:
     csv_reader = csv.reader(budget_file, delimiter = ',')
@@ -11,9 +13,12 @@ with open(csvpath) as budget_file:
 
     for row in csv_reader:
         month_total = (month_total + 1)
+        net_total = (net_total + row[1])
+
 
 print(
     f"Financial Analysis\n"
     f"------------------------------------------\n"
     f"Total Months: {month_total}\n"
+    f"Total: {net_total}\n"
 )
