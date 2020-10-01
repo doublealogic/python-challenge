dfc322d184af694df2c8f93f2fb0deadfb940b03
doublealogic <cras.ajrobertson@gmail.com>
2020-09-30 22:07:25 -0400

    Updated PyPoll script to calculate unique candidates, each candidate's total votes, percentage of vote each candidate won and the winner based on popular vote

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,9 +1,13 @@
 import os
 import csv
 
+candidates = {}
+
 elec_csvpath = os.path.join('..', 'PyPoll', 'Resources', 'election_data.csv')
 
 vote_total = 0
+winning_votes = 0
+winner = None
 
 with open(elec_csvpath) as election_file:
     elec_csvreader = csv.reader(election_file, delimiter = ',')
@@ -11,11 +15,28 @@ with open(elec_csvpath) as election_file:
     elec_csvheader = next(election_file)
 
     for row in elec_csvreader:
-        vote_total = (vote_total + 1)
+        vote_total += 1
+        if row[2] not in candidates:
+            candidates[row[2]] = 0
+        candidates[row[2]] += 1
 
 print(
     f'Election Results\n'
     f'-----------------------------------\n'
     f'Total Votes: {vote_total}\n'
-    f'-----------------------------------\n'
+    f'-----------------------------------'
+)
+
+for candidate,total_can_votes in candidates.items():
+    vote_percentage = (round((total_can_votes/vote_total), 3) * 100)
+    if total_can_votes >= winning_votes:
+        winning_votes = total_can_votes
+        winner = candidate
+    print(
+        f'{candidate}: {vote_percentage}% ({total_can_votes})'
+    )
+
+print(
+    f'Winner: {winner}\n'
+    f'-----------------------------------'
 )
